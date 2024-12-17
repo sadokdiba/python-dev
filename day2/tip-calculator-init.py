8aaa7579ae5b47af1e34b22159833ab1ea37d451
Sadok Diba <sadokdiba@gmail.com>
2024-12-18 00:14:25 +0200

    Initial commit

---
+++ day2/tip-calculator-init.py
@@ -0,0 +1,10 @@
+print("Welcome to the tip calculator!")
+try:
+    bill_total = float(input("What was the total of the bill? $"))
+except:
+    print("Please enter a number")
+    quit()
+tip_percent_amount = float(input("How much tip would you like to give in %? 10, 12 or 15? "))
+split_bill_number = int(input("How many people to split the bill? "))
+final_amount_per_person = (((bill_total*tip_percent_amount)/100) + bill_total) /split_bill_number
+print("Each person should pay: $",round(final_amount_per_person,2))
