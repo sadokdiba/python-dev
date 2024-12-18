95d5644edc9e0b8768a5702af65cb533548bfa00
Sadok Diba <sadokdiba@gmail.com>
2024-12-18 21:37:51 +0200

    Initial commit of rollercoaster-check.py

---
+++ day3/rollercoaster-check.py
@@ -0,0 +1,10 @@
+import time
+print("Welcome to the rollercoaster!")
+time.sleep(1)
+
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("you can ride the rollercoaster")
+else:
+    print("Sorry you cannot ride the rollercoaster")
