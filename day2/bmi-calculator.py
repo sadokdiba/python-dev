34bf63954a33f14e028c3c1b4e0f45d1fc45ed1b
Sadok Diba <sadokdiba@gmail.com>
2024-12-18 20:49:17 +0200

    adding error handling to bmi-calculator.py

--- day2/bmi-calculator.py
+++ day2/bmi-calculator.py
@@ -1,9 +1,17 @@
 import time
 print("This is a calculator to help you calculate your BMI")
 time.sleep(1)
-weight = float(input("Please enter your weight in kilograms \n"))
-height = float(input("Please enter your height in in meters eg. 1.75m \n"))
+
+try:
+    weight = float(input("Please enter your weight in kilograms \n"))
+    height = float(input("Please enter your height in in meters eg. 1.75m \n"))
+except:
+    print("Enter a valid number")
+    quit()
+
 bmi_calculation = weight/height**2
+
 print("Calculating...")
 time.sleep(3) #pretending to calculate :)
-print(f"Your BMI is {bmi_calculation:.2f}")
+
+print("Your BMI is:", round(bmi_calculation))
