6fb36c3637ccab6ba0b5162868d2510cbe2610ff
Sadok Diba <sadokdiba@gmail.com>
2024-12-19 00:17:23 +0200

    Added more error handling to rollercoaster-check.py

--- day3/rollercoaster-check.py
+++ day3/rollercoaster-check.py
@@ -2,17 +2,54 @@ import time
 print("Welcome to the rollercoaster!")
 time.sleep(1)
 
+def photo_check(bill):
+    attempts =3
+    while attempts > 0:
+        wants_photo = input("Do you want to have a photo taken? Type 'y' for Yes and 'n' for No: ").strip().lower()
+        if wants_photo in ["y", "n"]:
+            break
+        else:
+            attempts -= 1
+            if attempts > 0:
+                print(f"Invalid input. Please type 'y' for Yes or 'n' for No.. {attempts} attempts left.")
+            else:
+                print("No attempts left. Exiting.")
+                break
+    
+    if wants_photo == "y":
+        bill += 3
+    
+    print(f"Your final bill is ${bill}")
+    return bill
+
 def agechecker():
     age = int(input("Please enter your age? "))
     if age <= 12:
-        print("you can ride the rollercoaster and you will pay $5")
+        bill = 5
+        print("Child tickets are  $5")
+
     elif age <= 18:
-        print("you can ride the rollercoaster and you will pay $10")
+        bill = 10
+        print("Youth tickets are $10")
     else:
-        print("you can ride the rollercoaster and you will pay $15")
+        bill = 15
+        print("adult tickets are $15")
+    photo_check(bill)
 
 def main():
-    height = int(input("Please enter your height: "))
+    attempts = 3
+    while attempts > 0:
+        try:
+            height = int(input("Please enter your height in cm: "))
+            break
+        except ValueError:
+            attempts -= 1
+            if attempts > 0:
+                print(f"Please enter a valid height. {attempts} attempts left.")
+            else:
+                print("No attempts left. Exiting.")
+                quit()
+      
     if height > 120:
         agechecker()
     else:
@@ -20,3 +57,4 @@ def main():
 
 if __name__== "__main__":
     main()
+    
