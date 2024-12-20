c8689aa69c87d7c90ad4ca9726b9dd5d899237ab
Sadok Diba <sadokdiba@gmail.com>
2024-12-20 11:32:39 +0200

    Add feature: display human choices when the results is anything other than a tie

--- day4/rock-paper-scissors-game.py
+++ day4/rock-paper-scissors-game.py
@@ -42,13 +42,17 @@ elif human_choice >= 3 or human_choice < 0 :
     print("You typed an invalid number. You lose!")
 
 elif human_choice == 0 and computer_choice == 2:
+    print(f"You chose:\n{list_of_choices[human_choice]}")
     print("You win!")
     
 elif computer_choice == 0 and human_choice == 2:
+    print(f"You chose:\n{list_of_choices[human_choice]}")
     print("You lose!")
     
 elif computer_choice > human_choice:
+    print(f"You chose:\n{list_of_choices[human_choice]}")
     print("You lose!")
     
 elif human_choice > computer_choice:
+    print(f"You chose:\n{list_of_choices[human_choice]}")
     print("You win!")
