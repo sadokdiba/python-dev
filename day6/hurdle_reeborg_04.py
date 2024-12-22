c9cd36eb9065fb03172ed166ebdc65d80af2b247
Sadok Diba <sadokdiba@gmail.com>
2024-12-22 02:05:37 +0200

    fixing indentation on hurdle-challenge-04

--- day6/hurdle_reeborg_04.py
+++ day6/hurdle_reeborg_04.py
@@ -2,15 +2,15 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
 
 def jump():
-        turn_left()
-        while wall_on_right():
-            move()
-        turn_right()
+    turn_left()
+    while wall_on_right():
         move()
-        turn_right()
-        while front_is_clear():
-            move()
-        turn_left()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
 
 def turn_right():
     n = 3
