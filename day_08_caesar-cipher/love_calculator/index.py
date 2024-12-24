111179694129e1f39a957db55a9452e4b8590574
Sadok Diba <sadokdiba@gmail.com>
2024-12-24 10:59:51 +0200

    docs: updating comments for better code readability

--- day_08_caesar-cipher/love_calculator/index.py
+++ day_08_caesar-cipher/love_calculator/index.py
@@ -1,10 +1,10 @@
 def calculate_love_score(name1, name2):
-    '''This function does the love calculation'''
+    '''Calculate the love score based on the names provided.'''
     name_combined = name1 + name2
     true_letters =['T','R','U','E']
     love_letters =['L','O','V','E']
 
-    #name_combined_length = len(name_combined)
+    # Calculate the score by counting occurrences of each target letter
     score1 = 0
     for letter in name_combined.upper():
         if letter in true_letters:
@@ -13,11 +13,13 @@ def calculate_love_score(name1, name2):
     for letter in name_combined.upper():
         if letter in love_letters:
             score2 += 1
+            
+    # Combine the two scores into a single love score
     lovescore = str(score1)+str(score2)
     print(f"Your love score is {lovescore}")
 
 def main():
-    '''Take input - this is the main function'''
+    '''Prompt the user for their names and calculate their love score.'''
     name1 = input("Enter your name: ").strip().upper()
     name2 = input("Enter your lover's name: ").strip().upper()
     calculate_love_score(name1, name2)
