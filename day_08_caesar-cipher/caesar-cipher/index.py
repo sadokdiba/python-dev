816f63413954e5721a3564ecf1409a19a4aaa333
Sadok Diba <sadokdiba@gmail.com>
2024-12-25 08:43:03 +0200

    fix/docs: fixed a for loop bug, added a readme file

--- day_08_caesar-cipher/caesar-cipher/index.py
+++ day_08_caesar-cipher/caesar-cipher/index.py
@@ -35,12 +35,12 @@ def get_valid_message():
     attempts = 3
     while attempts > 0:
         message = input("Type your message (must not contain numbers):\n").strip().lower()
-        if message.isalpha() or len(message) <= 32 or " " in message:
+        if message.replace(" ", "").isalpha() and len(message) <= 16: # "replace(" ", "")"" removes spaces before performing isalpha check
             return message
         else:
             attempts -= 1
             print(
-                f"Invalid input! Message must be a string without numbers and no more than 32 characters. You have {attempts} attempts left.")
+                f"Invalid input!\nMessage must be a string without numbers and no more than 16 characters including spaces.\nYou have {attempts} attempts left.")
 
     print("Too many invalid attempts. Goodbye.")
     quit()
@@ -58,22 +58,30 @@ def get_valid_shift():
     print("Too many invalid attempts. Goodbye.")
     quit()
 
-should_continue = True
+def main():
+    try:
+        should_continue = True
+        while should_continue:
+            
+            direction = get_valid_direction()
+            text = get_valid_message()
+            shift = get_valid_shift()
 
-while should_continue:
-    
-    direction = get_valid_direction()
-    text = get_valid_message()
-    shift = get_valid_shift()
+            caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
-    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+            restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").strip().lower()
+            if restart == "no":
+                should_continue = False
+                print("Goodbye")
+            elif restart == "yes":
+                continue
+            else:
+                should_continue = False
+                print("Invalid selection. Goodbye")
+    except KeyboardInterrupt:
+        print("You have exited the program: Good Bye")
+    except ValueError as e:
+        print(f"An error occured: {e}")
 
-    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").strip().lower()
-    if restart == "no":
-        should_continue = False
-        print("Goodbye")
-    elif restart == "yes":
-        continue
-    else:
-        should_continue = False
-        print("Invalid selection. Goodbye")
+if __name__ == "__main__":
+    main()
