77450402d014d34441824a6949d7c5853457199f
Sadok Diba <sadokdiba@gmail.com>
2024-12-26 06:46:32 +0200

    style: reformat code according to style guide

--- day_09_the-secret-auction/grading-program/grading_program.py
+++ day_09_the-secret-auction/grading-program/grading_program.py
@@ -8,14 +8,14 @@ student_scores = {
 
 student_grades = {}
 
-for grades in student_scores:
-    if 91 <= student_scores[grades] <= 100:
-        student_grades[grades] = "Outstanding"
-    elif 81 <= student_scores[grades] <= 90:
-        student_grades[grades] = "Exceeds Expectations"
-    elif 71 <= student_scores[grades] <= 80:
-        student_grades[grades] = "Acceptable"
+for key in student_scores:
+    if 91 <= student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif 81 <= student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif 71 <= student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
     else:
-        student_grades[grades] = "Fail"
+        student_grades[key] = "Fail"
         
 print(student_grades)
