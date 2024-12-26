0cd6740951316250dd374c49c216aa0b53b92fc3
Sadok Diba <sadokdiba@gmail.com>
2024-12-27 00:03:33 +0200

    refact/feat: refactoring and initial commit of the pythonistas calculator

---
+++ day-10_the-pythonista-calculator/leap_year_finder.py
@@ -0,0 +1,26 @@
+def is_leap_year(year):
+    ''' Check if year is leap'''
+    if year % 4 == 0 and year % 20 == 0 and year % 400 == 0:
+        return year == year
+    return False
+attempts = 3
+while attempts > 0:
+    try:
+        year_inputed = int(input("Enter the year you'd like to determine: \n"))
+        if year_inputed > 0:
+            is_year_leap = is_leap_year(year=year_inputed)
+            print(is_year_leap)
+            print(is_leap_year.__doc__)
+            quit()
+        elif year_inputed < 0:
+            print("Please enter a positive year number")
+            attempts -=1
+        else:
+            print(f"{ValueError}")
+    except KeyboardInterrupt:
+        print("Program ended.")
+        quit()
+    except ValueError as e:
+        print(f"An error occurred: {e}")
+        attempts -=1
+print("Too many invalid entries, try again")
---
+++ day-10_the-pythonista-calculator/the-pythonista-calculator/index.py
@@ -0,0 +1,99 @@
+import art
+
+print(art.logo)
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1,n2):
+    return n1 - n2
+def multiplication(n1,n2):
+    return n1 * n2
+def division(n1,n2):
+    return n1 / n2
+
+operations_dict = {
+    "+": add,
+    "-": subtract,
+    "*": multiplication,
+    "/": division
+}
+def clear_screen():
+    print("\n" * 50)
+
+def print_operations():
+    for k in operations_dict:
+        print(k)
+
+def operating_cell(n1,n2,op):
+    if op == "+":
+        result = operations_dict["+"](n1,n2)
+        return result
+    elif op == "-":
+        result = operations_dict["-"](n1,n2)
+        return result
+    elif op == "*":
+        result = operations_dict["*"](n1,n2)
+        return result
+    elif op == "/":
+        result = operations_dict["/"](n1, n2)
+        return result
+    else:
+        return ValueError
+    
+def pick_operation():
+    pick_an_operation = input("Pick an operation: ")
+    return pick_an_operation
+
+def next_number():
+    second_number = float(input("Whats the next number?: "))
+    return second_number
+
+def continue_calculating(result):
+    is_continue = True
+    while is_continue:
+        try:
+            continue_value = input(f"Type 'y' to continue calculating with {result:.9f}, Type 'n' to start a new calculation or type any key to exit ")
+            if continue_value == 'y':
+                clear_screen()
+                print_operations()
+                next_operation = pick_operation()
+                next_value = next_number()
+                new_result = operating_cell(n1=result,n2=next_value,op=next_operation)
+                result = new_result
+            elif continue_value == 'n':
+                clear_screen()
+                is_continue = False
+            else:
+                return False
+        except TypeError as e:
+            print(f"An error occured: {e}")
+        except ValueError as e:
+            print(f"An error occured: {e}")
+        except KeyboardInterrupt:
+            print("Keyboard exit!")
+            quit()
+    return True
+
+def main():
+    is_calculating = True
+    while is_calculating:
+        try:
+            first_number = float(input("What's the first number?: "))
+            print_operations()
+            operation = pick_operation()
+            second_number = next_number()
+            final_answer = operating_cell(n1=first_number,n2=second_number,op=operation)
+            print(f"{first_number:.2f} {operation} {second_number:.2f} = {final_answer:.9f}")
+            is_calculating = continue_calculating(result=final_answer)
+        except TypeError as e:
+            print(f"An error occured {e}")
+        except ValueError as e:
+            print(f"An error occured {e}")
+        except KeyboardInterrupt:
+            print("Keyboard exit!")
+            quit()
+
+    print("Thank you for using our program!\nGood bye")
+
+if __name__ == "__main__":
+    main()
