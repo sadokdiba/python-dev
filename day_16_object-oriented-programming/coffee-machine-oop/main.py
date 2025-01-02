c282c55510ff4d4602dff4bbdb12d1f7f44f4459
Sadok Diba <sadokdiba@gmail.com>
2025-01-03 00:19:22 +0200

    refactor/fix: fixing and refactoring function to be more more dynamic

--- day_16_object-oriented-programming/coffee-machine-oop/main.py
+++ day_16_object-oriented-programming/coffee-machine-oop/main.py
@@ -2,40 +2,44 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-def print_report():
+def print_report(moneymachine, coffeemaker):
     coffeemaker.report()
     moneymachine.report()
 
-menu = Menu()
-moneymachine = MoneyMachine()
-coffeemaker = CoffeeMaker()
+def handle_user_input(user_choice, menu, moneymachine, coffeemaker):
 
+    if user_choice == 'report':
+        print_report(moneymachine, coffeemaker)
+    elif user_choice == 'off':
+        return False
+    else:
+        drink = menu.find_drink(user_choice)
+        cost = drink.cost
+
+        if coffeemaker.is_resource_sufficient(drink):
+            print(f"The {drink.name} will cost: ${round(cost, 2)}")
+            
+            if moneymachine.make_payment(cost):
+                coffeemaker.make_coffee(drink)
+
+        return True
+    
 def main():
+    menu = Menu()
+    moneymachine = MoneyMachine()
+    coffeemaker = CoffeeMaker()
+
     machine_on = True
     invalid_inputs_remaining = 3
+
     while machine_on and invalid_inputs_remaining > 0:
         try:
-            user_choice = input(f"What would you like, {menu.get_items()}?")
+            user_choice = input(f"What would you like, {menu.get_items()}?").strip().lower()
 
-            if user_choice == 'report':
-                print_report()
-            elif user_choice == 'off':
-                machine_on = False
-            else:
-
-                cost = menu.find_drink(user_choice).cost
-                drink = menu.find_drink(user_choice)
-
-                if coffeemaker.is_resource_sufficient(drink):
-                
-                    menu.find_drink(user_choice)
-                    print(f"The {menu.find_drink(user_choice).name} will cost: ${round(menu.find_drink(user_choice).cost, 2)}")
-                    
-                    moneymachine.make_payment(cost)
-                    coffeemaker.make_coffee(drink)
+            machine_on = handle_user_input(user_choice, menu, moneymachine, coffeemaker)
                 
         except AttributeError as e:
-            print(f"you've enterred an invalid input ({e})")
+            print(f"Invalid input: {e}")
             invalid_inputs_remaining -= 1
         except KeyboardInterrupt:
             print("Machine forcefully turned off!")
