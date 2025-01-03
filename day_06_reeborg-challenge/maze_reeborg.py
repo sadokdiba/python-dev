4341a4f7e98ef452fdbb2590a397ee50072bf094
Sadok Diba <sadokdiba@gmail.com>
2025-01-03 12:55:35 +0200

    refact:...

--- day_06_reeborg-challenge/maze_reeborg.py
+++ day_06_reeborg-challenge/maze_reeborg.py
@@ -1,16 +1,16 @@
-# # this code will run only at link below:
-# # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
-#
-# def turn_right():
-#     n = 3
-#     for _ in range(n):
-#         turn_left()
-#
-# while not at_goal():
-#     if right_is_clear:
-#         turn_right()
-#         move()
-#     elif front_is_clear():
-#         move()
-#     else:
-#         turn_left()
+# this code will run only at link below:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    n = 3
+    for _ in range(n):
+        turn_left()
+
+while not at_goal():
+    if right_is_clear:
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
