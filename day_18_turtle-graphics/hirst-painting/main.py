defb81dcdf9166f0e5ad5d89358e11e155105b5d
Sadok Diba <sadokdiba@gmail.com>
2025-01-06 22:11:12 +0200

    style: removing white spaces and unecessary commented codes

--- day_18_turtle-graphics/hirst-painting/main.py
+++ day_18_turtle-graphics/hirst-painting/main.py
@@ -1,23 +1,10 @@
 import random
 import turtle
-import random
 
 tim = turtle.Turtle()
 turtle.colormode(255)
 tim.hideturtle()
-#
-# rgb_colors = []
-#
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
-# 20 dot size - 50 paces
+
 color_list = [(1, 9, 30), (121, 95, 41), (72, 32, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 170), (151, 92, 115), (35, 90, 26), (6, 154, 73), (205, 63, 91), (168, 129, 78), (3, 78, 28), (1, 64, 147), (4, 220, 218), (80, 135, 179), (130, 157, 177), (81, 110, 135), (120, 187, 164), (11, 213, 220), (118, 18, 36), (243, 205, 7), (132, 223, 209), (229, 173, 165), (221, 179, 218)]
 
 def draw_ten_dots():
@@ -36,10 +23,6 @@ def main():
         y += 40
 
 main()
-
-
-
-
 
 
 screen = turtle.Screen()
