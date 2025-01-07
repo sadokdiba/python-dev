d6e90ad0d928b37c1e8398f4c022372ddaf99366
Sadok Diba <sadokdiba@gmail.com>
2025-01-07 08:25:31 +0200

    feat:docs/style/chore: adding v2 of hirst painting, some more chore and style work

--- projects/day_18_turtle-graphics/hirst-painting-v2/main.py
+++ projects/day_18_turtle-graphics/hirst-painting-v2/main.py
@@ -4,25 +4,28 @@ import turtle
 tim = turtle.Turtle()
 turtle.colormode(255)
 tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(270)
+tim.setheading(0)
+tim_dots = 100
+
 
 color_list = [(1, 9, 30), (121, 95, 41), (72, 32, 21), (238, 212, 72), (220, 81, 59), (226, 117, 100), (93, 1, 21), (178, 140, 170), (151, 92, 115), (35, 90, 26), (6, 154, 73), (205, 63, 91), (168, 129, 78), (3, 78, 28), (1, 64, 147), (4, 220, 218), (80, 135, 179), (130, 157, 177), (81, 110, 135), (120, 187, 164), (11, 213, 220), (118, 18, 36), (243, 205, 7), (132, 223, 209), (229, 173, 165), (221, 179, 218)]
 
-def draw_ten_dots():
-    for _ in range(10):
-        tim.dot(20, random.choice(color_list))
-        tim.penup()
-        tim.forward(50)
-        tim.pendown()
-def main():
-    x = -250
-    y = -250
-    for _ in range(10):
-        tim.penup()
-        tim.setposition(x, y)
-        draw_ten_dots()
-        y += 40
 
-main()
+for dots in range(1, tim_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dots % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
 
 
 screen = turtle.Screen()
--- projects/day_18_turtle-graphics/hirst-painting/main.py
+++ projects/day_18_turtle-graphics/hirst-painting/main.py
@@ -14,13 +14,12 @@ def draw_ten_dots():
         tim.forward(50)
         tim.pendown()
 def main():
-    x = -250
-    y = -250
+    x = -250, y = -250
     for _ in range(10):
         tim.penup()
         tim.setposition(x, y)
         draw_ten_dots()
-        y += 40
+        y += 45
 
 main()
 
