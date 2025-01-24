08954d8d0b27d6fdc1cb6b6c2c3a67818ab0003f
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-01-25 00:52:52 +0200

    feat: list comprehension lessons complete

--- projects/day_26_NATO-alphabet_list-comprehension/test/test.py
+++ projects/day_26_NATO-alphabet_list-comprehension/test/test.py
@@ -9,4 +9,40 @@ result = [common for common in file1_list if common in file2_list]
 
 print(result)
 
+# ######
 
+import random
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+students_scores = {student:random.randint(1, 100) for student in names}
+for v in students_scores.values():
+    print(f"{v} \n")
+passed_students = {k:v for k,v in students_scores.items() if v > 50}
+for v in passed_students.keys():
+    print(v)
+
+# #####
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+words_list = sentence.split()
+result = {word:len(word) for word in words_list}
+for v in result.values():
+    print(v)
+
+# #####
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {day:(temp_c * 9/5) + 32 for day,temp_c in weather_c.items()}
+print(weather_f)
+# ####
+
+import pandas as pd
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_dataframe = pd.DataFrame(student_dict)
+print(student_dataframe)
+
+for (index, row) in student_dataframe.iterrows():
+    print(row.score)
