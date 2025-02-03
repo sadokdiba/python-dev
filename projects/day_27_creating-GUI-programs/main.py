9c767acebee7dbc57f4d639467f82e0752ce1ff2
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-03 23:28:56 +0200

    refact: CI/CD tkinter

---
+++ projects/day_27_creating-GUI-programs/main.py
@@ -0,0 +1,22 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI PROGRAM")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="Testing Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
