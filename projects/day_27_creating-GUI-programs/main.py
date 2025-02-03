95563b657afc115cb81e8be1c9dbbb65fbee9d17
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-04 00:18:51 +0200

    refact: playground args commit

--- projects/day_27_creating-GUI-programs/main.py
+++ projects/day_27_creating-GUI-programs/main.py
@@ -4,8 +4,8 @@ window = tkinter.Tk()
 window.title("GUI PROGRAM")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Testing Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = tkinter.Label(text="Testing Label", font=("Arial", 24))
+my_label.pack(side="left")
 
 
 
---
+++ projects/day_27_creating-GUI-programs/playground.py
@@ -0,0 +1,6 @@
+def add(*args):
+    args_var = [n for n in args]
+    print(sum(args_var))
+
+
+add(3, 5, 8, 9)
