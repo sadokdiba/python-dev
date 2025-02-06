c1d2577749ea08ea141bc2c7ce21061f4d6e2e48
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-06 08:25:05 +0200

    refact: adding solution for merge sorted array

--- leetcode-interview-solutions/merge-sorted-array/main.py
+++ leetcode-interview-solutions/merge-sorted-array/main.py
@@ -7,4 +7,49 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+# real world solution:
+
+class SolutionReal:
+    def __init__(self, nums1, m, nums2, n):
+        self.nums1 = nums1
+        self.m = m
+        self.nums2 = nums2
+        self.n = n
+
+    def merge(self):
+        i, j, k = self.m - 1, self.n - 1, self.m + self.n - 1
+        while i >= 0 and j >= 0:
+            if self.nums1[i] > self.nums2[j]:
+                self.nums1[k] = self.nums1[i]
+                i -= 1
+            else:
+                self.nums1[k] = self.nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            self.nums1[k] = self.nums2[j]
+            j -= 1
+            k -= 1
+    
+sol = SolutionReal([1,2,3,0,0,0], 3, [2,5,6], 3)
+sol.merge()
+print(sol.nums1)  
+
--- projects/day_27_creating-GUI-programs/playground.py
+++ projects/day_27_creating-GUI-programs/playground.py
@@ -1,6 +1,18 @@
-def add(*args):
-    args_var = [n for n in args]
-    print(sum(args_var))
+# def add(*args):
+#     args_var = [n for n in args]
+#     print(sum(args_var))
 
 
-add(3, 5, 8, 9)
+# add(3, 5, 8, 9)
+
+# ####
+
+# def calculate (n, **kwargs):
+#     print(kwargs)
+
+# calculate(2, add=2, substract=3)
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
