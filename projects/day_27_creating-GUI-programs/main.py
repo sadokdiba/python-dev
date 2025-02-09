f2ebf02f1e3541152bb61845195dcd401820fb81
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-09 17:16:47 +0200

    refact: adding TKinter code

--- projects/day_27_creating-GUI-programs/main.py
+++ projects/day_27_creating-GUI-programs/main.py
@@ -1,17 +1,27 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("GUI PROGRAM")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="Testing Label", font=("Arial", 24))
-my_label.pack(side="left")
+my_label = Label(text="Testing Label", font=("Arial", 16, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+def button_clicked():
+    print("I got clicked!")
+    new_text = input.get()
+    my_label.config(text=f"{new_text}")
 
 
+button = Button(text="Click Me", font=("Arial", 24), command=button_clicked)
+button.pack()
 
-
-
-
+input = Entry(width=10)
+input.pack()
+# print(input.get())
 
 
 
