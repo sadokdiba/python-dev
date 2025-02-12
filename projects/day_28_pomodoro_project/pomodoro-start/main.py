8f7b9885360c622a270925afa900ffe72f3770bf
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-13 00:18:36 +0200

    refact: CI/CD pomodoro

--- projects/day_28_pomodoro_project/pomodoro-start/main.py
+++ projects/day_28_pomodoro_project/pomodoro-start/main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import time
+import math
+
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,7 +17,24 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,13 +44,14 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
 
 title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 title.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
@@ -38,6 +59,8 @@ reset_button.grid(column=2, row=2)
 
 check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
+
 
 
 
