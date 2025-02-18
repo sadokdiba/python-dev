dfd937da87bd22cbdb92b66fac64941adda9e9d6
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-02-18 09:59:24 +0200

    refact: CI/CD pw managed

--- projects/day_29_tkinter-password-manager/password-manager-start/main.py
+++ projects/day_29_tkinter-password-manager/password-manager-start/main.py
@@ -5,8 +5,17 @@ import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_entry.delete(0, END)
+    password = ""
+    for _ in range(20):
+        password += random.choice(letters)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -16,20 +25,20 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=0, row=0, columnspan=3)
+canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
+website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
 
 email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2, sticky="ew")
+email_label.grid(column=0, row=2)
 
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
+email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, " ")
 email_entry.focus()
 
@@ -37,13 +46,13 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3, sticky="ew")
+password_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Password")
-generate_button.grid(column=2, row=3, sticky="ew")
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36)
-add_button.grid(column=1, row=4, columnspan=2, sticky="ew")
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
 
