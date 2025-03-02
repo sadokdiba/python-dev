ef3d48daad176fa942306fc0f3772580e1a99edb
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-03-02 23:43:52 +0200

    refact: changing timing structure

--- projects/day_28_pomodoro_project/pomodoro-start/main.py
+++ projects/day_28_pomodoro_project/pomodoro-start/main.py
@@ -12,9 +12,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 35
+SHORT_BREAK_MIN = 10
+LONG_BREAK_MIN = 25
 reps = 0
 timer = None
 
