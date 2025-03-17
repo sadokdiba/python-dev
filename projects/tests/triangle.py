442d76fbd4682d8ded7f087a7aae97f423603d4b
Sadok Diba <98810666+sadokdiba@users.noreply.github.com>
2025-03-17 11:28:27 +0200

    feat: adding tests

---
+++ projects/tests/triangle.py
@@ -0,0 +1,20 @@
+# # solution1
+# def draw_triangle():
+#     t = ""
+#     for x in range(0, 5):
+#         for y in range(0, x + 1):
+#             t = t + "*"
+#         t = t + "\n"
+#     print(t)
+# draw_triangle()
+# print("finish drawing triangle")
+
+#solution2
+
+def draw_triangle():
+    t = ""
+    for x in range(0, 5):
+        t = t + "*"
+        print(t)
+draw_triangle()
+print("finish drawing triangle")
